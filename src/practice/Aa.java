370fc5d4f1209f99facac5a9b4b9c536459238ba
Jyoti <jyoti.patra09@gmail.com>
2021-06-29 22:54:00 -0400

    commit3

--- src/practice/Aa.java
+++ src/practice/Aa.java
@@ -8,8 +8,16 @@ public class Aa {
 	//private Aa() {}
 	
 public static void main(String[] args) {
-	Aa a=new Aa();
-	Aa a1=new Aa();
+String s="hello hi";
+String []s1=s.split("");
+
+String []s2=s.split(" ");
+for(int i=0;i<s1.length;i++) {
+	System.out.print(s1[i]+" ");
+}
+for(int i=0;i<s2.length;i++) {
+	System.out.print(s2[i]+"  ");
+}
 
 		
 }
